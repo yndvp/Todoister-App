253f402ad5fe1bfe9bc2adeb457d9927fc2cb41b
yndvp <58580623+yndvp@users.noreply.github.com>
2022-03-07 20:35:00 -0500

    implement saving task and due date from dialog

--- app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
+++ app/src/main/java/com/bawp/todoister/BottomSheetFragment.java
@@ -1,18 +1,44 @@
 package com.bawp.todoister;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.bawp.todoister.model.Priority;
+import com.bawp.todoister.model.Task;
+import com.bawp.todoister.model.TaskViewModel;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.chip.Chip;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.Group;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class BottomSheetFragment extends BottomSheetDialogFragment {
+    private EditText enterTodo;
+    private ImageButton calenderButton;
+    private ImageButton priorityButton;
+    private ImageButton saveButton;
+    private RadioGroup priorityRadioGroup;
+    private RadioButton selectedRadioButton;
+    private int selectedButtonId;
+    private CalendarView calendarView;
+    private Group calenderGroup;
+    private Date dueDate;
+    Calendar calendar = Calendar.getInstance();
 
     public BottomSheetFragment(){
 
@@ -25,11 +51,54 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     ) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.bottom_sheet, container, false);
+        calenderGroup = view.findViewById(R.id.calendar_group);
+        calendarView = view.findViewById(R.id.calendar_view);
+        calenderButton = view.findViewById(R.id.today_calendar_button);
+        enterTodo = view.findViewById(R.id.enter_todo_et);
+        saveButton = view.findViewById(R.id.save_todo_button);
+        priorityButton = view.findViewById(R.id.priority_todo_button);
+        priorityRadioGroup = view.findViewById(R.id.radioGroup_priority);
+
+        Chip todayChip = view.findViewById(R.id.today_chip);
+        Chip tomorrowChip = view.findViewById(R.id.tomorrow_chip);
+        Chip nextWeekChip = view.findViewById(R.id.next_week_chip);
+
         return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        calenderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calenderGroup.setVisibility(
+                        calenderGroup.getVisibility() == View.GONE ?
+                        View.VISIBLE : View.GONE);
+            }
+        });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                calendar.clear();
+                calendar.set(year, month, dayOfMonth);
+                dueDate = calendar.getTime();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String task = enterTodo.getText().toString().trim();
+                if(!TextUtils.isEmpty(task) && dueDate != null) {
+                    Task myTask = new Task(task, Priority.HIGH,
+                            dueDate, Calendar.getInstance().getTime(),
+                            false);
+                    TaskViewModel.insert(myTask);
+                }
+            }
+        });
 
     }
 }
